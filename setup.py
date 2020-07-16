cf9668251043280088b4bb58f3fcdc428562835e
Richardo Otros <richardo.el.otros@gmail.com>
2020-07-16 18:33:49 +0200

    added requirements to setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name='Shush',
 
-    version='0.1dev',
+    version='0.1.0',
     packages=find_packages(),
     license='GNU General Public License v3.0',
     long_description=open('README.md').read(),
+    install_requires=[
+        'spidev',
+        'RPi.GPIO',
+    ]
 )
